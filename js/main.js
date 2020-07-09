7e9a92429d88e998079f87f68029a7680df62257
andrea.longhitano <andrea.longhitano@mail.polimi.it>
2020-07-09 19:26:01 +0200

    Important function for raycsting

--- js/main.js
+++ js/main.js
@@ -155,10 +155,26 @@ function animate(){
     window.requestAnimationFrame(updateScene);
   };
 
-  function drawScene() {
+  function resize(canvas) {
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+   
+    // Check if the canvas is not the same size.
+    if (canvas.width  != displayWidth ||
+        canvas.height != displayHeight) {
+   
+      // Make the canvas the same size
+      canvas.width  = displayWidth;
+      canvas.height = displayHeight;
+    }
+  }
 
+  function drawScene() {
+    resize(gl.canvas);
     gl.clearColor(0.85, 0.85, 0.85, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     animate();
     computeViewMatrix();
     showAxes();
